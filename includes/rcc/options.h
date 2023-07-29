17f04048c42c1dfba7327b0521c95d100048842f
luoyin <912271673@qq.com>
2023-07-29 14:17:58 +0800

    update

---
+++ includes/rcc/options.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<types.h>
+#include<stdalign.h>
+
+enum step{
+    preprocess,
+    compile,
+    assemble,
+    link
+};
+
+struct opts{
+    alignas(i32) u8 opt_level;
+    alignas(i32) enum step _step;
+    alignas(i32) u32 file_c;
+    alignas(i64) char** file_p;
+    alignas(i32) bool rename;
+    alignas(i64) char* name;
+};
+
+void get_opts(struct opts * value);
